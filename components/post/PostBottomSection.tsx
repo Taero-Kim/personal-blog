3a3e33375d729d05a3f12d25d0d88080258eaab3
Taero-Kim <taero30@gmail.com>
2022-08-16 16:57:23 +0900

    posting browser-rendering

--- components/post/PostBottomSection.tsx
+++ components/post/PostBottomSection.tsx
@@ -10,7 +10,7 @@ const PostBottomSection = ({ data }: PostTopSectionProps) => {
     <div>
       <ShareLink data={data} />
       <div className="relative flex h-[110px] justify-center bg-gray20 dark:bg-darkTag">
-        <div className="absolute top-[-21px] flex w-full max-w-[1080px] justify-end px-4 md:px-0">
+        <div className="absolute top-[-25px] flex w-full max-w-[1080px] justify-end px-4 md:px-0">
           <div className="itmes-end mr-4 flex flex-col justify-end">
             <div className="ml-auto font-bold leading-[26px] text-black60 dark:text-darkText">
               Taero
@@ -19,7 +19,9 @@ const PostBottomSection = ({ data }: PostTopSectionProps) => {
               꾸준히 성장하고 싶은 개발자입니다.
             </div>
           </div>
-          <div className="h-[100px] w-[100px] bg-black"></div>
+          <div className="h-[100px] w-[100px] rounded-full bg-white">
+            <img src="/images/salad.png" alt="main-logo" />
+          </div>
         </div>
       </div>
     </div>
